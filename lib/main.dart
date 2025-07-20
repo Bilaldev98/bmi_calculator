2df536e16e16af6cc60e0c8efd2610021044a8b6
M Bilal <bilalm1108335@gmail.com>
2025-07-20 15:53:05 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,8 @@ class BMICalculator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFFFFFFFF),
-      ),
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(primaryColor: Color(0xFFFFFFFF)),
       home: InputPage(),
     );
   }
@@ -36,8 +34,54 @@ class _InputPageState extends State<InputPage> {
         ),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: const Center(
-        child: Text('BodyText'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(),
+          ),
+
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
