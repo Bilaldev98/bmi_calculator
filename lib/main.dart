7ba1645dc6442e6b6ec2de8977c1a5a711242704
M Bilal <bilalm1108335@gmail.com>
2025-07-20 17:23:47 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -69,20 +69,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.color});
-
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
-  }
-}
