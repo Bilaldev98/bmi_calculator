69bf53dd3607d2935fa32733dd33622f25c51525
M Bilal <bilalm1108335@gmail.com>
2025-07-21 20:14:47 +0500

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ const activeCardColor = Color(0xFF1D1E33);
 const bottomContainerColor = Color(0xFFEB1555);
 const inactiveCardColor = Color(0xFF111328);
 
+enum Gender{
+  male,
+  female,
+}
+
 void main() => runApp(const BMICalculator());
 
 class BMICalculator extends StatelessWidget {
@@ -34,8 +39,8 @@ class _InputPageState extends State<InputPage> {
   Color maleCardColor = inactiveCardColor;
   Color femaleCardColor = inactiveCardColor;
 
-  void updateColor(int gender){
-    if(gender ==1){
+  void updateColor(Gender selectedGender){
+    if(selectedGender == Gender.male){
       if(maleCardColor == inactiveCardColor){
         maleCardColor =activeCardColor;
         femaleCardColor = inactiveCardColor;
@@ -44,7 +49,7 @@ class _InputPageState extends State<InputPage> {
       }
     }
 
-    if(gender ==2){
+    if(selectedGender == Gender.female){
       if(femaleCardColor == inactiveCardColor){
         femaleCardColor = activeCardColor;
         maleCardColor = inactiveCardColor;
@@ -78,7 +83,7 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: (){
                       setState(() {
-                        updateColor(1);
+                        updateColor(Gender.male);
                       });
                     },
                     child: ReusableCard(
@@ -95,7 +100,7 @@ class _InputPageState extends State<InputPage> {
                   child: GestureDetector(
                     onTap: (){
                       setState(() {
-                        updateColor(2);
+                        updateColor(Gender.female);
                       });
                     },
                     child: ReusableCard(
